4e27d5e72506472640034723fb7550d8921ea318
findxc <liuxiaocui1993@gmail.com>
2022-08-03 18:22:56 +0800

    add prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,41 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('jestcoverageofcurrentfile.run', () => {
-		const document = vscode.window.activeTextEditor?.document;
-		if(!document) {
-			return;
-		}
-		const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
-		if(!workspaceFolder) {
-			return;
-		}
+  const disposable = vscode.commands.registerCommand(
+    'jestcoverageofcurrentfile.run',
+    () => {
+      const document = vscode.window.activeTextEditor?.document
+      if (!document) {
+        return
+      }
+      const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri)
+      if (!workspaceFolder) {
+        return
+      }
 
-		const testFile = document.fileName.replace(`${workspaceFolder.uri.fsPath}/`, '');
-		// transform xxx.spec.ts or xxx.test.tsx to xxx.ts*
-		// xxx.ts* can match xxx.ts and xxx.tsx
-		const collectFrom = testFile.replace(/\.(spec|test)(\.[tj]s)x?$/, '$2*');
+      const testFile = document.fileName.replace(
+        `${workspaceFolder.uri.fsPath}/`,
+        ''
+      )
+      // transform xxx.spec.ts or xxx.test.tsx to xxx.ts*
+      // xxx.ts* can match xxx.ts and xxx.tsx
+      const collectFrom = testFile.replace(/\.(spec|test)(\.[tj]s)x?$/, '$2*')
 
-		let terminal = vscode.window.terminals.find(item => item.name === 'jest-coverage' && !item.exitStatus);
-		if(!terminal) {
-			terminal = vscode.window.createTerminal('jest-coverage');
-		}
+      let terminal = vscode.window.terminals.find(
+        item => item.name === 'jest-coverage' && !item.exitStatus
+      )
+      if (!terminal) {
+        terminal = vscode.window.createTerminal('jest-coverage')
+      }
 
-		terminal.show();
-		terminal.sendText(`npm test -- "${testFile}" "--watchAll=false" "--coverage" "--collectCoverageFrom=${collectFrom}"`);
-	});
+      terminal.show()
+      terminal.sendText(
+        `npm test -- "${testFile}" "--watchAll=false" "--coverage" "--collectCoverageFrom=${collectFrom}"`
+      )
+    }
+  )
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate() {}
